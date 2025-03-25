21853497b512cdfe41f11414839a9fdd770cbb5b
szymonmelait <250346@edu.p.lodz.pl>
2025-03-25 16:28:01 +0100

    tests reupload

---
+++ MelaAndGradGroup/Tests/controllersTests/ProductControllerTests.cs
@@ -0,0 +1,81 @@
+using MelaAndGradGroup.onlineShopProgram.controllers;
+using MelaAndGradGroup.onlineShopProgram.services;
+using Moq;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+public class ProductControllerTests
+{
+    private readonly Mock<IProductService> _productServiceMock;
+    private readonly ProductController _productController;
+
+    public ProductControllerTests()
+    {
+        _productServiceMock = new Mock<IProductService>();
+        _productController = new ProductController(_productServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAllProducts_ShouldReturnOkWithProducts()
+    {
+        // Arrange
+        var products = new List<Product> { new Product("Test", 10, 1, "Description") };
+        _productServiceMock.Setup(s => s.FindAll()).ReturnsAsync(products);
+
+        // Act
+        var result = await _productController.GetAllProducts();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<List<Product>>(okResult.Value);
+        Assert.Equal(products, returnValue);
+    }
+
+    [Fact]
+    public async Task GetProductById_ShouldReturnOkWithProduct()
+    {
+        // Arrange
+        var product = new Product("Test", 10, 1, "Description");
+        _productServiceMock.Setup(s => s.FindById(It.IsAny<int>())).ReturnsAsync(product);
+
+        // Act
+        var result = await _productController.GetProductById(1);
+
+        // Assert
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<Product>(okResult.Value);
+        Assert.Equal(product, returnValue);
+    }
+
+    [Fact]
+    public async Task GetProductById_ShouldReturnNotFound()
+    {
+        // Arrange
+        _productServiceMock.Setup(s => s.FindById(It.IsAny<int>())).ReturnsAsync((Product)null);
+
+        // Act
+        var result = await _productController.GetProductById(1);
+
+        // Assert
+        Assert.IsType<NoContentResult>(result);
+    }
+
+    [Fact]
+    public async Task AddProduct_ShouldReturnCreatedAtAction()
+    {
+        // Arrange
+        var productDTO = new ProductDTO("Test", 10, 1, "Description");
+        _productServiceMock.Setup(s => s.AddProduct(productDTO)).ReturnsAsync(new Product("Test", 10, 1, "Description"));
+
+        // Act
+        var result = await _productController.CreateProduct(productDTO);
+
+        // Assert
+        var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
+        Assert.Equal(nameof(_productController.GetProductById), createdAtActionResult.ActionName);
+    }
+}
---
+++ MelaAndGradGroup/Tests/servicesTests/ProductServiceTests.cs
@@ -0,0 +1,60 @@
+using MelaAndGradGroup.onlineShopProgram.services;
+using MelaAndGradGroup.onlineShopProgram.repositories;
+using Moq;
+using Xunit;
+using System.Threading.Tasks;
+
+public class ProductServiceTests
+{
+    private readonly Mock<IProductRepository> _repositoryMock;
+    private readonly ProductService _productService;
+
+    public ProductServiceTests()
+    {
+        _repositoryMock = new Mock<IProductRepository>();
+        _productService = new ProductService(_repositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task AddProduct_ShouldCallRepositorySave()
+    {
+        // Arrange
+        var productDTO = new ProductDTO("Test", 10, 1, "Description");
+
+        // Act
+        await _productService.AddProduct(productDTO);
+
+        // Assert
+        _repositoryMock.Verify(r => r.Save(It.IsAny<Product>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task FindAll_ShouldCallRepositoryFindAll()
+    {
+        // Arrange
+        var products = new List<Product> { new Product("Test", 10, 1, "Description") };
+        _repositoryMock.Setup(r => r.FindAll()).ReturnsAsync(products);
+
+        // Act
+        var result = await _productService.FindAll();
+
+        // Assert
+        _repositoryMock.Verify(r => r.FindAll(), Times.Once);
+        Assert.Equal(products, result);
+    }
+
+    [Fact]
+    public async Task FindById_ShouldCallRepositoryFindById()
+    {
+        // Arrange
+        var product = new Product("Test", 10, 1, "Description");
+        _repositoryMock.Setup(r => r.FindByID(It.IsAny<int>())).ReturnsAsync(product);
+
+        // Act
+        var result = await _productService.FindById(1);
+
+        // Assert
+        _repositoryMock.Verify(r => r.FindByID(1), Times.Once);
+        Assert.Equal(product, result);
+    }
+}
