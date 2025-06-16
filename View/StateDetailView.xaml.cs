89848397bc7389ff8363b0148f276325f8612980
szymonmelait <250346@edu.p.lodz.pl>
2025-06-16 21:51:21 +0200

    state view added

---
+++ View/StateDetailView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace View
+{
+    public partial class StateDetailView : UserControl
+    {
+        public StateDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ View/StateMasterView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using ViewModel;
+
+namespace View
+{
+    public partial class StateMasterView : UserControl
+    {
+        public StateMasterView()
+        {
+            InitializeComponent();
+            DataContext = new StateListViewModel();  
+        }
+    }
+}
