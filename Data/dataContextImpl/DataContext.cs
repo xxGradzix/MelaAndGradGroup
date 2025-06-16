cf4106e180d43319ec930e947984d7d4418b1ca1
szymonmelait <250346@edu.p.lodz.pl>
2025-06-16 23:44:08 +0200

    code cleanup

--- Data/dataContextImpl/DataContext.cs
+++ Data/dataContextImpl/DataContext.cs
@@ -31,127 +31,24 @@ namespace Data.dataContextImpl
                 _context = new AppDbContext(GetInMemoryOptions());
             }
         }
-        // public void AddCatalog(int id, string name, double price, string description)
-        // {
-        //     // Catalog c = new Catalog(catalogs.Count, name, price, description);
-        //     Catalog c = new Catalog(id, name, price, description);
-        //     catalogs.Add(c);
-        // }
-        // public void AddUser(int id, string username, string password, string email, string phoneNumber)
-        // {
-        //     // User u = new User(users.Count, username, password, email, phoneNumber);
-        //     User u = new User(id, username, password, email, phoneNumber);
-        //     users.Add(u);
-        // }
-        //
-        // public void AddState(int id, int nrOfProducts, int catalogId)
-        // {
-        //     State s = new State(states.Count, nrOfProducts, GetCatalogFromId(catalogId));
-        //     states.Add(s);
-        // }
-        //
-        //
-        // public void AddUserEvent(int id, int stateId, int userId)
-        // {
-        //     // UserEvent e = new UserEvent(events.Count, GetStateFromId(stateId), GetUserFromId(userId));
-        //     UserEvent e = new UserEvent(id, GetStateFromId(stateId), GetUserFromId(userId));
-        //     events.Add(e);
-        // }
-        // public void AddDatabaseEvent(int id, int stateId)
-        // {
-        //     // DatabaseEvent e = new DatabaseEvent(events.Count, GetStateFromId(stateId));
-        //     DatabaseEvent e = new DatabaseEvent(id, GetStateFromId(stateId));
-        //     events.Add(e);
-        // }
-        //
-        // public void ChangeState(int stateId, int change)
-        // {
-        //     GetStateFromId(stateId).nrOfProducts += change;
-        // }
-        //
-        // public IUser? GetUser(int id)
-        // {
-        //     var entity = (from User
-        //             in c.Users
-        //         where User.userId == id
-        //         select User).FirstOrDefault();
-        //     if (entity == null)
-        //     {
-        //         return null;
-        //     }
-        //     else
-        //     {
-        //         return new LibraryUser(entity.userId, entity.firstName, entity.lastName);
-        //     }
-        // }
-        //
-        // public override IEvent GetEventFromId(int id)
-        // {
-        //     foreach (Event e in events)
-        //     {
-        //         if (e.eventId == id) return e;
-        //     }
-        //     throw new Exception("No event with id: " + id + " found in database.");
-        // }
-        //
-        // public override IState GetStateFromId(int id)
-        // {
-        //     foreach (State state in states)
-        //     {
-        //         if (state.stateId == id) return state;
-        //     }
-        //     throw new Exception("No state with id: " + id + " found in database.");
-        // }
-        //
-        // public override ICatalog GetCatalogFromId(int id)
-        // {
-        //     foreach (Catalog catalog in catalogs)
-        //     {
-        //         if (catalog.id == id) return catalog;
-        //     }
-        //     throw new Exception("No catalog with id: " + id + " found in database.");
-        // }
-        //
-        // public override List<IUser> GetUsers()
-        // {
-        //     List<IUser> userList = new List<IUser>();
-        //     foreach (User user in users)
-        //     {
-        //         userList.Add(user);
-        //     }
-        //     return userList;
-        // }
-        //
-        // public override List<ICatalog> getProducts()
-        // {
-        //     List<ICatalog> catalogList = new List<ICatalog>();
-        //     foreach (Catalog catalog in catalogs)
-        //     {
-        //         catalogList.Add(catalog);
-        //     }
-        //     return catalogList;
-        // }
+
         public List<ICatalog> GetAllCatalog()
         {
-            // return catalogs.Values.Cast<ICatalog>().ToList();
             return _context.Products.Cast<ICatalog>().ToList();
         }
 
         public List<IUser> GetAllUser()
         {
-            // return users.Cast<IUser>().ToList();
             return _context.Users.Cast<IUser>().ToList();
         }
 
         public List<IEvent> GetAllEvent()
         {
-            // return events.Cast<IEvent>().ToList();
             return _context.Events.Cast<IEvent>().ToList();
         }
 
         public List<IState> GetAllState()
         {
-            // return states.Cast<IState>().ToList();
             return _context.States.Cast<IState>().ToList();
         }
 
@@ -160,13 +57,7 @@ namespace Data.dataContextImpl
             var existing = _context.Products.FirstOrDefault(c => c.id == id);
             if (existing != null)
             {
-                // Możesz zaktualizować istniejący katalog lub rzucić wyjątek
                 throw new Exception($"Catalog with id {id} already exists.");
-                // Lub, jeśli chcesz zaktualizować:
-                // existing.name = name;
-                // existing.price = price;
-                // existing.description = description;
-                // _context.Products.Update(existing);
             }
             else
             {
@@ -179,7 +70,6 @@ namespace Data.dataContextImpl
         public void AddUser(int id, string username, string password, string email, string phoneNumber)
         {
             User u = new User(id, username, password, email, phoneNumber);
-            // users.Add(u);
             _context.Users.Add(u);
             _context.SaveChanges();
         }
@@ -188,7 +78,6 @@ namespace Data.dataContextImpl
         public void AddEvent(int id, int stateId)
         {
             Event e = new Event(id, (State)GetState(stateId));
-            // events.Add(e);
             _context.Events.Add(e);
             _context.SaveChanges();
         }
@@ -196,9 +85,7 @@ namespace Data.dataContextImpl
         public void AddState(int id, int nrOfProducts, int catalogId)
         {
             State s = new State(id, nrOfProducts, (Catalog)GetCatalog(catalogId));
-            // states.Add(s);
             _context.States.Add(s);
-            
             _context.SaveChanges();
         }
 
@@ -217,78 +104,39 @@ namespace Data.dataContextImpl
 
         public void RemoveUser(int userId)
         {
-            // var user = GetUser(userId);
-            // if (user != null)
-            // {
-            //     users.Remove((User)user);
-            // }
-            // else
-            // {
-            //     throw new Exception("No user with id: " + userId + " found in database.");
-            // }
             var user = GetUser(userId);
             if (user == null)
             {
                 throw new Exception("No user with id: " + userId + " found in database.");
             }
-            // users.Remove((User)user);
             _context.Users.Remove((User)user);
             _context.SaveChanges();
         }
 
         public void RemoveEvent(int eventId)
         {
-            // var eventItem = GetEvent(eventId);
-            // if (eventItem != null)
-            // {
-            //     events.Remove((Event)eventItem);
-            // }
-            // else
-            // {
-            //     throw new Exception("No event with id: " + eventId + " found in database.");
-            // }
             var eventItem = GetEvent(eventId);
             if (eventItem == null)
             {
                 throw new Exception("No event with id: " + eventId + " found in database.");
             }
-            // events.Remove((Event)eventItem);
             _context.Events.Remove((Event)eventItem);
             _context.SaveChanges();
         }
 
         public void RemoveState(int stateId)
         {
-            // var state = GetState(stateId);
-            // if (state != null)
-            // {
-            //     states.Remove((State)state);
-            // }
-            // else
-            // {
-            //     throw new Exception("No state with id: " + stateId + " found in database.");
-            // }
             var state = GetState(stateId);
             if (state == null)
             {
                 throw new Exception("No state with id: " + stateId + " found in database.");
             }
-            // states.Remove((State)state);
             _context.States.Remove((State)state);
             _context.SaveChanges();
         }
 
         public void ChangeState(int stateId, int change)
         {
-            // var state = GetState(stateId);
-            // if (state != null)
-            // {
-            //     state.nrOfProducts += change;
-            // }
-            // else
-            // {
-            //     throw new Exception("No state with id: " + stateId + " found in database.");
-            // }
             var state = GetState(stateId);
             if (state == null)
             {
@@ -302,11 +150,6 @@ namespace Data.dataContextImpl
 
         public ICatalog? GetCatalog(int id)
         {
-            // if (catalogs.TryGetValue(id, out var catalog))
-            // {
-                // return catalog;
-            // }
-            // return null;
             var entity = _context.Products.FirstOrDefault(c => c.id == id);
             if (entity == null)
             {
@@ -321,7 +164,6 @@ namespace Data.dataContextImpl
 
         public IUser? GetUser(int id)
         {
-            // return users.FirstOrDefault(u => u.id == id);
             var entity = _context.Users.FirstOrDefault(u => u.id == id);
             if (entity == null)
             {
@@ -335,7 +177,6 @@ namespace Data.dataContextImpl
 
         public IEvent? GetEvent(int id)
         {
-            // return events.FirstOrDefault(e => e.eventId == id);
             var entity = _context.Events.FirstOrDefault(e => e.Id == id);
             if (entity == null)
             {
@@ -349,7 +190,6 @@ namespace Data.dataContextImpl
 
         public IState? GetState(int id)
         {
-            // return states.FirstOrDefault(s => s.stateId == id);
             var entity = _context.States.FirstOrDefault(s => s.Id == id);
             if (entity == null)
             {
@@ -364,10 +204,6 @@ namespace Data.dataContextImpl
 
         public void TruncateData()
         {
-            // catalogs.Clear();
-            // events.Clear();
-            // users.Clear();
-            // states.Clear();
             _context.Products.RemoveRange(_context.Products);
             _context.Users.RemoveRange(_context.Users);
             _context.Events.RemoveRange(_context.Events);
